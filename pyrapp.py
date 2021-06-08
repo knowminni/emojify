f272d1b6afa610d26cafe4914b1796fc8a3cd974
Neha Vishwakarma <envyneha1998.1999@gmail.com>
2021-06-08 15:57:52 +0530

    Add files via upload

--- pyrapp.py
+++ pyrapp.py
@@ -1,20 +1,28 @@
 import emoji
 import streamlit as st
+from alphafy import *
 
 est = [':lemon:', ':crown:', ':pizza:', ':kissing_cat:', ':hamburger:',
  ':sparkles:', ':fire:', ':crescent_moon:']
 emolst = [emoji.emojize(i) for i in est]
 
 
+def evaluate(text, ch):
+      for i in text:
+            st.write(eval(i + '(ch)'))
+      
+
 st.title('Emoji Text Editor')
 st.header('Write a Text > Choose your Emoji > Hit on the Button')
 st.subheader('Fact: I got annoyed of all the spamming but somebody shot this idea.')
 
 
-text = st.text_input('Writeth thy W\'rd: ', max_chars = 10)
-choice = st.select_slider(label='Chooseth thy Emoji', options = emolst)
+tmp = st.text_input('Writeth thy W\'rd: ', max_chars = 10)
+text = tmp.replace(' ', '').upper()
+choice = st.selectbox(label='Chooseth thy Emoji', options = emolst)
 
-print(choice)
+if st.button('Letss Goooo to the Mooon '+emoji.emojize(':sunglasses:')):
+      evaluate(text, choice)
 
 
 
